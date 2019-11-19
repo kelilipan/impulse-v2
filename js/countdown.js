7c1c7cca5ee390c443ad62b060ccbc1d60746390
raisoturu <anvaqta@student.telkomuniversity.ac.id>
2019-11-20 00:04:24 +0700

    Asprak dashboard pages

---
+++ js/countdown.js
@@ -0,0 +1,126 @@
+//circle start
+let progressBar = document.querySelector(".e-c-progress");
+let indicator = document.getElementById("e-indicator");
+let pointer = document.getElementById("e-pointer");
+let length = Math.PI * 2 * 100;
+
+progressBar.style.strokeDasharray = length;
+
+function update(value, timePercent) {
+  var offset = -length - (length * value) / timePercent;
+  progressBar.style.strokeDashoffset = offset;
+  pointer.style.transform = `rotate(${(360 * value) / timePercent}deg)`;
+}
+
+//circle ends
+const displayOutput = document.querySelector(".display-remain-time");
+const pauseBtn = document.getElementById("pause");
+const setterBtns = document.querySelectorAll("button[data-setter]");
+
+let intervalTimer;
+let timeLeft;
+let wholeTime = 60 * 60; // manage this to set the whole time
+let isPaused = false;
+let isStarted = false;
+
+update(wholeTime, wholeTime); //refreshes progress bar
+displayTimeLeft(wholeTime);
+
+function changeWholeTime(seconds) {
+  if (wholeTime + seconds > 0) {
+    wholeTime += seconds;
+    update(wholeTime, wholeTime);
+  }
+}
+
+for (var i = 0; i < setterBtns.length; i++) {
+  setterBtns[i].addEventListener("click", function(event) {
+    var param = this.dataset.setter;
+    switch (param) {
+      case "minutes-plus":
+        changeWholeTime(1 * 60);
+        break;
+      case "minutes-minus":
+        changeWholeTime(-1 * 60);
+        break;
+      case "seconds-plus":
+        changeWholeTime(1);
+        break;
+      case "seconds-minus":
+        changeWholeTime(-1);
+        break;
+    }
+    displayTimeLeft(wholeTime);
+  });
+}
+
+function timer(seconds) {
+  //counts time, takes seconds
+  let remainTime = Date.now() + seconds * 1000;
+  displayTimeLeft(seconds);
+
+  intervalTimer = setInterval(function() {
+    timeLeft = Math.round((remainTime - Date.now()) / 1000);
+    if (timeLeft < 0) {
+      clearInterval(intervalTimer);
+      isStarted = false;
+      setterBtns.forEach(function(btn) {
+        btn.disabled = false;
+        btn.style.opacity = 1;
+      });
+      displayTimeLeft(wholeTime);
+      pauseBtn.classList.remove("disabled");
+      //   pauseBtn.classList.add("play");
+      return;
+    }
+    displayTimeLeft(timeLeft);
+  }, 1000);
+}
+notifyStart = () => {
+  Swal.fire({
+    position: "bottom-end",
+    icon: "success",
+    backdrop: false,
+    text: "Practicum successfully started",
+    showConfirmButton: false,
+    timer: 1800,
+    timerProgressBar: true
+  });
+};
+function pauseTimer(event) {
+  if (isStarted === false) {
+    timer(wholeTime);
+    isStarted = true;
+    // this.classList.remove("play");
+    notifyStart();
+    this.classList.add("disabled");
+    setterBtns.forEach(function(btn) {
+      btn.disabled = true;
+      btn.style.opacity = 0.5;
+    });
+  } else if (isPaused) {
+    notifyStart();
+    // this.classList.remove("play");
+    this.classList.add("disabled");
+    timer(timeLeft);
+    isPaused = isPaused ? false : true;
+  } else {
+    this.classList.remove("disabled");
+    // this.classList.add("play");
+    clearInterval(intervalTimer);
+    isPaused = isPaused ? false : true;
+  }
+}
+
+function displayTimeLeft(timeLeft) {
+  //displays time on the input
+  let minutes = Math.floor(timeLeft / 60);
+  let seconds = timeLeft % 60;
+  let displayString = `${minutes < 10 ? "0" : ""}${minutes}:${
+    seconds < 10 ? "0" : ""
+  }${seconds}`;
+  displayOutput.textContent = displayString;
+  update(timeLeft, wholeTime);
+}
+
+pauseBtn.addEventListener("click", pauseTimer);
